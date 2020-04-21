576e30726f72b7598ebfdeaf7aad5c642f9a1661
Maciej <masobkowiak@gmail.com>
2020-04-22 01:11:05 +0200

    .

--- src/Web/src/App.tsx
+++ src/Web/src/App.tsx
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
-import {
-    BrowserRouter as Router,
-    Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router";
 import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
-import { Hello } from "./components/Hello/Hello";
+//import { Hello } from "./components/Hello/Hello";
+//import { Menu } from "./components/Menu/Menu";
+
+import { NavButton } from "./components/Menu/NavButton";
 
 export default class App extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Router>
-                    <BreadcrumbsProvider>
-                        <Switch>
-                            <Route
-                                path={"/"}
-                                component={Hello}
-                            />
-                        </Switch>
-                    </BreadcrumbsProvider>
-                </Router>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <BreadcrumbsProvider>
+            <Switch>
+              <Route path={"/"} component={NavButton} />
+            </Switch>
+          </BreadcrumbsProvider>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
---
+++ src/Web/src/components/Menu/NavButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {
+  name: string;
+  forwardTo: string;
+}
+
+export const NavButton: React.FC<Props> = () => {
+  return (
+    <>
+      <button></button>
+    </>
+  );
+};
